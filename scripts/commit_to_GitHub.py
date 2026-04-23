503c1e683b3fada33539954d7e290e34da8f2824
ia-lozano <130255731+ia-lozano@users.noreply.github.com>
2026-04-22 18:47:13 -0600

    removed useless lines

--- scripts/commit_to_GitHub.py
+++ scripts/commit_to_GitHub.py
@@ -1,10 +1,11 @@
 import subprocess
 
 #Pushing changes to master branch
+BRANCH = "master"
 commands = [
     ["git", "add", "."],
     ["git", "commit", "-m", "removed useless lines"],
-    ["git", "push", "-u", "origin", "master"]
+    ["git", "push", "-u", "origin", BRANCH]
 ]
 
 for cmd in commands:
